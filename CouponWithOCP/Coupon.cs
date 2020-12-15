287daec5d9825695671c79f4c0276778412618b2
asus <trias.2001@students.amikom.ac.id>
2020-12-15 22:58:49 +0700

    Add project files.

---
+++ CouponWithOCP/Coupon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithOCP
+{
+    public abstract class Coupon
+    {
+        public abstract double calculate(double originPrice);
+    }
+}
